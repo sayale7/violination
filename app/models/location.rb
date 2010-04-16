1c9a8072095c157df43a0acf52e640291fd9b044
sayale7 <thomas.kohler@gmx.com>
2010-04-16 20:05:20 +0200

    maps

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ActiveRecord::Base
   
+  #acts_as_mappable :default_units  => :kms
+  
   attr_accessible :lat, :lng, :distance, :taggable_type, :taggable_id
   belongs_to :taggable, :polymorphic => true
-  
-  
   
 end
