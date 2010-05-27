45715a9a7c4cdc355f924767c6342bc618cea850
sayale7 <thomas.kohler@gmx.com>
2010-05-27 14:25:00 +0200

    google fuer instrumente

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     session[:search_input] = nil
+    show_on_map
   end
   
   def show
@@ -75,6 +76,23 @@ class UsersController < ApplicationController
     @available_tags = Tag.find_all_by_taggable_type_and_parent_id('User', nil) - @added_tags
     @photo = Photo.new
     @photos = Photo.find_all_by_photo_container_id_and_thumbnail_and_photo_container_type(@the_instance.id, nil, 'User')
+  end
+  
+  private
+  
+  def show_on_map
+    @map = GMap.new("map_div")
+    @map.control_init(:large_map => true, :map_type => true)
+    @map.center_zoom_init([49.5874362000,10.9660867000],5)
+    User.all.each do |user|
+      if !(user.location.lat.nil? || user.location.lng.nil?)
+        if user.full_name.to_s.eql?(' ')
+          @map.overlay_init(GMarker.new([user.location.lat,user.location.lng], :title => user.username, :info_window => "<a href=/users/#{user.id}>#{user.username}</a> <br/> <p>#{user.location.address}</p>"))
+        else
+          @map.overlay_init(GMarker.new([user.location.lat,user.location.lng], :title => user.full_name, :info_window => "<a href=/users/#{user.id}>#{user.full_name}</a> <br/> <p>#{user.location.address}</p>"))
+        end
+      end
+    end
   end
   
 end
