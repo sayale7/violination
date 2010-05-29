076edb4c42f08d0bfb798b149a4f00b3aa7143ad
sayale7 <thomas.kohler@gmx.com>
2010-05-29 14:35:18 +0200

    javascript updates

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -141,7 +141,7 @@ class ItemsController < ApplicationController
                 location_array.push(GMarker.new([item.location.lat,item.location.lng], :icon => group_icon, :info_window => "<p>#{item.location.address}</p> <p>#{t('common.more')}</p> <p><a href='/items?contact=1&taggable_type=#{item.item_type}&user_id=#{item.user.id}'>#{t('common.show')}</a></p>"))
             end
           end
-          @map.overlay_init(Clusterer.new(location_array,  :max_visible_markers => 2))
+          @map.overlay_init(Clusterer.new(location_array,  :max_visible_markers => 100, :min_markers_per_cluster => 100))
         end
       end
     end
