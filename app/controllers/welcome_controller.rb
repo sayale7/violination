f9bbe005a96ad6ab6009ad9fd2ff53b0597aa9e1
sayale7 <thomas.kohler@gmx.com>
2010-05-31 21:50:51 +0200

    site admin

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   
   def index
     show_on_map
+    render :layout => '/layouts/application_2'
   end
   
   private
@@ -9,7 +10,7 @@ class WelcomeController < ApplicationController
   def show_on_map
     @map = GMap.new("map_div")
     @map.control_init(:large_map => true, :map_type => true)
-    @map.center_zoom_init([49.5874362000,10.9660867000],5)
+    @map.center_zoom_init([49.800499,13.080481],5)
     User.all.each do |user|
       if !(user.location.lat.nil? || user.location.lng.nil?)
         if user.full_name.to_s.eql?(' ')
