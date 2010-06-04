98dd341ef5a6fa4ef8416ab2f2f36ff08165079d
sayale7 <thomas.kohler@gmx.com>
2010-06-04 12:15:29 +0200

    add map keys to plugin ym4r

--- vendor/plugins/ym4r_gm/lib/gm_plugin/key.rb
+++ vendor/plugins/ym4r_gm/lib/gm_plugin/key.rb
@@ -12,8 +12,18 @@ module Ym4r
       unless File.exist?(RAILS_ROOT + '/config/gmaps_api_key.yml')
         raise GMapsAPIKeyConfigFileNotFoundException.new("File RAILS_ROOT/config/gmaps_api_key.yml not found")
       else
-        env = ENV['RAILS_ENV'] || RAILS_ENV
-        GMAPS_API_KEY = YAML.load_file(RAILS_ROOT + '/config/gmaps_api_key.yml')[env]
+        #env = ENV['RAILS_ENV'] || RAILS_ENV
+        #load manually key since we have subdomains
+        if request.url.to_s.include?('www.')
+          GMAPS_API_KEY = "ABQIAAAAuhwT7uBsmRtiCbPsilW88BSxsGsNsGaGFua-aUakP3VKKmziVxQj7zvABvmGYoBzb3RxcDPu94lEVg"
+        elsif request.subdomains.first.to_s.eql?('de')
+          GMAPS_API_KEY = "ABQIAAAAuhwT7uBsmRtiCbPsilW88BR46FPUkB9-w9KRp9IxPnme1fEpjxSmPbrTE6jSjtfu0PSbhohiqR9YuQ"
+        elsif request.subdomains.first.to_s.eql?('en')
+          GMAPS_API_KEY = "ABQIAAAAuhwT7uBsmRtiCbPsilW88BQeiR_yjAgcy3oysuNqEOJBZwhEARTFeQZPi4edN9q3yGeYyw2m6vKn2Q"
+        else
+          GMAPS_API_KEY = "ABQIAAAAuhwT7uBsmRtiCbPsilW88BSxsGsNsGaGFua-aUakP3VKKmziVxQj7zvABvmGYoBzb3RxcDPu94lEVg"
+        end
+        #GMAPS_API_KEY = YAML.load_file(RAILS_ROOT + '/config/gmaps_api_key.yml')[env]
       end
       
       def self.get(options = {})
