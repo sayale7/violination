299b8138c18373b8299b8caeeb8fce675e23120b
sayale7 <thomas.kohler@gmx.com>
2010-06-04 15:51:31 +0200

    locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,12 +24,12 @@ class ApplicationController < ActionController::Base
   
   def english
     I18n.default_locale = 'en'
-    redirect_to root_url
+    redirect_to request.url
   end
   
   def german
     I18n.default_locale = 'de'
-    redirect_to root_url
+    redirect_to reques.url
   end
   
   def set_default_url_for_mails
