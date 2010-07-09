ff0b33773c5ed5c4692aac56d4c4e414c7024016
Thomas Kohler <thomaskohler@Thomas-Kohlers-MacBook-Pro.local>
2010-07-09 11:04:18 +0200

    google maps api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,12 +27,13 @@ class ApplicationController < ActionController::Base
   
   def english
     #I18n.default_locale = 'en'
-    redirect_to "http://en.#{request.domain}"
+    request.url = 
+    redirect_to "http://en.#{request.domain << request.request_uri}"
   end
   
   def german
     #I18n.default_locale = 'de'
-    redirect_to "http://de.#{request.domain}"
+    redirect_to "http://de.#{request.domain << request.request_uri}"
   end
   
   def set_default_url_for_mails
