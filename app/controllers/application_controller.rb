684088390c842fbb51c7e70059b8e511e33e77da
Thomas Kohler <thomaskohler@Thomas-Kohlers-MacBook-Pro.local>
2010-07-09 10:33:07 +0200

    google maps api key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
-  #before_filter :set_locale
+  before_filter :set_locale
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
